56fad61ffb37c3feb1119af422d785414f821155
Renato Filizzola <rfilizzola@funcionalcorp.com.br>
2020-05-28 12:54:51 -0300

    Creating component class

---
+++ src/JwtComponent.php
@@ -0,0 +1,31 @@
+<?php 
+
+use CApplicationComponent;
+use fidelize\JwtAuthKeys\JwtAuth;
+
+class JwtComponent extends CApplicationComponent 
+{
+    public $secret;
+    public $keysDirectory;
+    protected $jwtAuth;
+
+    public function init()
+    {
+        $this->jwtAuth = new JwtAuth();
+        $this->jwtAuth->setSecret($this->secret);
+        $this->jwtAuth->setKeysDirectory($this->keysDirectory);
+        
+        parent::init();
+    }
+
+    public function encode($payload)
+    {
+        return $this->jwtAuth->encode($payload);
+    }
+
+    public function decode($msg)
+    {
+        return $this->jwtAuth->decode($msg);
+    }
+
+}
